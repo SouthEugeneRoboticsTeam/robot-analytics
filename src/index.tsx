2f7aaca46c890e793de225ded42ce4e6a1771ff1
Aidan Heller <asfheller@gmail.com>
2018-10-02 20:23:41 -0700

    Make appbar and drawer work

--- src/index.tsx
+++ src/index.tsx
@@ -2,25 +2,18 @@ import * as React from 'react';
 import { render } from 'react-dom'
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
 import { App } from './components/App';
 import { store } from './state/store';
 import './firebase';
-import { addScout, addTeam } from './state/actions';
-
-//test
-store.dispatch(addTeam('Sert', 2521));
-store.dispatch(addTeam('ChezyPoofs', 254));
-store.dispatch(addScout({
-    name: 'Helo',
-    templateName: 'Hai',
-    team: store.getState().teams[0],
-    sections: []
-}));
+import { theme } from './theme';
 
 const Root = () => (
     <BrowserRouter>
         <Provider store={store}>
-            <App />
+            <MuiThemeProvider theme={theme}>
+                <App />
+            </MuiThemeProvider>
         </Provider>
     </BrowserRouter>
 );
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,11 +1,135 @@
 import * as React from 'react';
+import classNames from 'classnames';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography';
 import Drawer from '@material-ui/core/Drawer';
-import { TeamListContainer } from "../containers/TeamListContainer";
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import { Theme } from '@material-ui/core/styles/createMuiTheme';
+import createStyles from '@material-ui/core/styles/createStyles'
+import withStyles, { WithStyles } from '@material-ui/core/styles/withStyles';
 
-export const Home = () => (
-    <>
-        <Drawer variant="permanent" >
-            <TeamListContainer />
-        </Drawer>
-    </>
+const drawerWidth = 240;
+
+const styles = (theme: Theme) => createStyles({
+    root: {
+        display: 'flex'
+    },
+    toolbar: {
+        paddingRight: 24, // keep right padding when drawer closed
+    },
+    toolbarIcon: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'flex-end',
+        padding: '0 8px',
+        ...theme.mixins.toolbar,
+    },
+    appBar: {
+        zIndex: theme.zIndex.drawer + 1,
+        transition: theme.transitions.create(['width', 'margin'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+    },
+    appBarShift: {
+        marginLeft: drawerWidth,
+        width: `calc(100% - ${drawerWidth}px)`,
+        transition: theme.transitions.create(['width', 'margin'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+    },
+    menuButton: {
+        marginLeft: 12,
+        marginRight: 36,
+    },
+    menuButtonHidden: {
+        display: 'none',
+    },
+    drawerPaper: {
+        position: 'relative',
+        whiteSpace: 'nowrap',
+        width: drawerWidth,
+        transition: theme.transitions.create('width', {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+    },
+    drawerPaperClose: {
+        overflowX: 'hidden',
+        transition: theme.transitions.create('width', {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+        width: theme.spacing.unit * 7,
+        [theme.breakpoints.up('sm')]: {
+            width: theme.spacing.unit * 9,
+        },
+    }
+});
+
+export const Home = withStyles(styles)(
+    class extends React.Component<HomeProps, HomeState> {
+        state = {
+            drawerOpened: false
+        };
+
+        handleDrawerOpen = () => this.setState({ drawerOpened: true });
+
+        handleDrawerClose = () => this.setState({ drawerOpened: false });
+
+        render() {
+            const { classes } = this.props;
+            const { drawerOpened } = this.state;
+            return (
+                <div className={classes.root}>
+                    <Drawer
+                        variant="persistent"
+                        classes={{
+                            paper: classNames(classes.drawerPaper, !drawerOpened && classes.drawerPaperClose)
+                        }}
+                        open={drawerOpened}
+                    >
+                        <div className={classes.toolbarIcon}>
+                            <IconButton onClick={this.handleDrawerClose}>
+                                <ChevronLeftIcon />
+                            </IconButton>
+                        </div>
+                        <Divider />
+                    </Drawer>
+                    <AppBar className={classNames(classes.appBar, drawerOpened && classes.appBarShift)}>
+                        <Toolbar disableGutters={!drawerOpened} className={classes.toolbar}>
+                            <IconButton
+                                aria-label="Open drawer"
+                                color="inherit"
+                                onClick={this.handleDrawerOpen}
+                                className={classNames(
+                                    classes.menuButton,
+                                    drawerOpened && classes.menuButtonHidden,
+                                )}
+                            >
+                                <MenuIcon />
+                            </IconButton>
+                            <Typography
+                                variant="title"
+                                color="inherit"
+                            >
+                                Robot Analytics
+                            </Typography>
+                        </Toolbar>
+                    </AppBar>
+                </div>
+            )
+        }
+    }
 );
+
+interface HomeProps extends WithStyles<typeof styles> {}
+
+interface HomeState {
+    drawerOpened: boolean
+}
