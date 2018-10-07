b1fa57c6c21c2594b84f02caaced392e353451a1
Aidan Heller <asfheller@gmail.com>
2018-10-07 01:08:54 -0700

    Delete unnecessary centralized Home state

--- src/state/actions.ts
+++ src/state/actions.ts
@@ -7,12 +7,6 @@ export const enum ActionTypes {
     ADD_SCOUT_TEMPLATE = 'ADD_SCOUT_TEMPLATE'
 }
 
-export enum HomeActionTypes {
-    SET_DRAWER_OPENED = 'SET_DRAWER_OPENED'
-}
-
 export const addTeam = (name: String, number: Number) => action(ActionTypes.ADD_TEAM, { name, number });
 export const addScout = (scout: Scout) => action(ActionTypes.ADD_SCOUT, scout);
 export const addScoutTemplate = (template: Scout) => action(ActionTypes.ADD_SCOUT_TEMPLATE, template);
-
-export const setDrawerOpened = (value: Boolean) => action(HomeActionTypes.SET_DRAWER_OPENED, value);
