3899d53124e7f13a81ca4fa70fac00f76b6be623
Aidan Heller <asfheller@gmail.com>
2018-10-13 17:00:30 -0700

    Improve metric typings

--- src/data/metric.ts
+++ src/data/metric.ts
@@ -14,3 +14,33 @@ export enum ScoutMetricType {
     BOOLEAN = "boolean",
     ENUM = "enum",
 }
+
+export interface TextMetric extends Metric<string> {
+    type: ScoutMetricType.TEXT
+}
+
+export interface NumberMetric extends Metric<number> {
+    type: ScoutMetricType.NUMBER
+}
+
+export interface NumberArrayMetric extends Metric<Array<number>> {
+    type: ScoutMetricType.NUMBER_ARRAY
+}
+
+export interface BooleanMetric extends Metric<boolean> {
+    type: ScoutMetricType.BOOLEAN
+}
+
+export interface EnumMetric extends Metric<string> {
+    type: ScoutMetricType.TEXT
+}
+
+export const isTextMetric = (metric: Metric): metric is TextMetric => metric.type === 'text';
+
+export const isNumberMetric = (metric: Metric): metric is NumberMetric => metric.type === 'number';
+
+export const isNumberArrayMetric = (metric: Metric): metric is NumberArrayMetric => metric.type === 'numberArray';
+
+export const isBooleanMetric = (metric: Metric): metric is BooleanMetric => metric.type === 'text';
+
+export const isEnumMetric = (metric: Metric): metric is EnumMetric => metric.type === 'text';
