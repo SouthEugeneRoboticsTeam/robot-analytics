dd86a33bf985b52334a598e8c2bc898ab26eac51
Aidan Heller <asfheller@gmail.com>
2018-11-06 18:20:03 -0800

    Get rid of examples

--- src/index.tsx
+++ src/index.tsx
@@ -8,56 +8,6 @@ import { store } from './state/store';
 import './firebase';
 import { theme } from './theme';
 
-// Test code TODO remove this before merging!
-import { addGame, addTeam } from './state/actions';
-import { ScoutMetricType } from './data/metric';
-
-store.dispatch(addGame('PowerUp', {
-    'Cubes in Scale': { type: ScoutMetricType.NUMBER, value: 0 },
-    'Cubes in Switch': { type: ScoutMetricType.NUMBER, value: 0 },
-    'Cubes in Exchange': { type: ScoutMetricType.NUMBER, value: 0 }
-}));
-
-store.dispatch(addTeam('SERT', 2521, {
-    'Q1': {
-        gameName: 'PowerUp',
-        sections: {
-            'Teleop': {
-                metrics: {
-                    'Cubes in Scale': { type: ScoutMetricType.NUMBER, value: 4 },
-                    'Cubes in Switch': { type: ScoutMetricType.NUMBER, value: 6 },
-                    'Cubes in Exchange': { type: ScoutMetricType.NUMBER, value: 1 }
-                }
-            }
-        }
-    },
-    'Q2': {
-        gameName: 'PowerUp',
-        sections: {
-            'Teleop': {
-                metrics: {
-                    'Cubes in Scale': { type: ScoutMetricType.NUMBER, value: 5 },
-                    'Cubes in Switch': { type: ScoutMetricType.NUMBER, value: 6 },
-                    'Cubes in Exchange': { type: ScoutMetricType.NUMBER, value: 1 }
-                }
-            }
-        }
-    },
-    'Q3': {
-        gameName: 'PowerUp',
-        sections: {
-            'Teleop': {
-                metrics: {
-                    'Cubes in Scale': { type: ScoutMetricType.NUMBER, value: 9 },
-                    'Cubes in Switch': { type: ScoutMetricType.NUMBER, value: 6 },
-                    'Cubes in Exchange': { type: ScoutMetricType.NUMBER, value: 1 }
-                }
-            }
-        }
-    }
-}));
-// Do it!
-
 const Root = () => (
     <BrowserRouter>
         <Provider store={store}>
