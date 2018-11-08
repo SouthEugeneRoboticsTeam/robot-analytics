fc169fbd53edf7f435907348aed9134fbfe25190
unknown <whou125@gmail.com>
2018-11-08 00:15:39 -0800

    Actually use single quotes instead of double

--- src/processing/addTeams.ts
+++ src/processing/addTeams.ts
@@ -1,11 +1,11 @@
-import { addTeams } from "./scoutDataFormatter"
+import { addTeams } from './scoutDataFormatter'
 // TODO: JSON importinng through UI
 import * as data from './scout.json'
 import { store } from '../state/store'
 
 // TODO: Maybe make changing this possible through the UI
 const preferences = {
-    gameName : "frc2018powerup"
+    gameName : 'frc2018powerup'
 }
 
-addTeams(data["teams"], preferences.gameName)
+addTeams(data['teams'], preferences.gameName)
