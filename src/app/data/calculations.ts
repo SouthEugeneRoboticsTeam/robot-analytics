99e4f865c83eaa03854ef455cac1f12bb74966c7
Andrew Dassonville <dassonville.andrew@gmail.com>
2018-12-09 19:56:51 -0800

    Use arrow functions
    Co-Authored-By: will-hou <whou125@gmail.com>

--- src/app/data/calculations.ts
+++ src/app/data/calculations.ts
@@ -1,6 +1,6 @@
 import { Metric, ScoutMetricType } from '@robot-analytics/data/metric';
 import { reduce } from 'lodash';
-import { std, round } from 'mathjs'
+import { std, round } from 'mathjs';
 
 export type Calculations = {
     [name: string]: Calculation
@@ -14,14 +14,14 @@ export interface Calculation<T = any> {
 export const calculations: Calculations = {
     'Maximum': {
         type: ScoutMetricType.NUMBER,
-        invoke: (...metrics: Array<Metric>) => reduce(metrics, function (acc: Metric, metric) {
+        invoke: (...metrics: Array<Metric>) => reduce(metrics, (acc: Metric, metric) => {
             if (metric.value > acc.value) acc.value = metric.value
             return acc
         }, { type: ScoutMetricType.NUMBER, value: 0, category: metrics[0].category})
     },
     'Minimum': {
         type: ScoutMetricType.NUMBER,
-        invoke: (...metrics: Array<Metric>) => reduce(metrics, function(acc: Metric, metric) {
+        invoke: (...metrics: Array<Metric>) => reduce(metrics, (acc: Metric, metric) => {
             if (metric.value > -1 && acc.value == -1) acc.value = metric.value
             else if (metric.value < acc.value) acc.value = metric.value
             return acc
@@ -29,13 +29,13 @@ export const calculations: Calculations = {
     },
     'Average': {
         type: ScoutMetricType.NUMBER,
-        invoke: (...metrics: Array<Metric>) => ({type:ScoutMetricType.NUMBER, value: round(reduce(metrics, function (acc, metric){ 
+        invoke: (...metrics: Array<Metric>) => ({type:ScoutMetricType.NUMBER, value: round(reduce(metrics, (acc, metric) => { 
             return acc + metric.value}, 0) / metrics.length, 2), category: metrics[0].category
         })
     },
     'Standard Deviation': {
         type: ScoutMetricType.NUMBER,
-        invoke: (...metrics: Array<Metric>) => ({type: ScoutMetricType.NUMBER, value: round(std(reduce(metrics, function(acc, metric) { 
+        invoke: (...metrics: Array<Metric>) => ({type: ScoutMetricType.NUMBER, value: round(std(reduce(metrics, (acc, metric) => { 
             acc.push(metric.value)
             return acc
             }, [])), 2), category: metrics[0].category})
