d3bb831d8b7d50b856cdc006fd992d06bb240363
Andrew Dassonville <dassonville.andrew@gmail.com>
2018-12-10 10:38:26 -0800

    Update calculations.ts

--- src/app/data/calculations.ts
+++ src/app/data/calculations.ts
@@ -1,6 +1,6 @@
 import { Metric, ScoutMetricType } from '@robot-analytics/data/metric';
 import { reduce } from 'lodash';
-import { std, round, median, mode, max, min } from 'mathjs';
+import { std, round, median, mean, mode, max, min } from 'mathjs';
 
 export type Calculations = {
     [name: string]: Calculation
@@ -16,43 +16,48 @@ export const calculations: Calculations = {
         type: ScoutMetricType.NUMBER,
         invoke: (...metrics: Array<Metric>) => ({
             type: ScoutMetricType.NUMBER,
-            value: max(metrics.map((metric) => metric.value})),
+            value: max(metrics.map((metric) => metric.value)),
             category: metrics[0].category,
-        })
+        }),
     },
     'Minimum': {
         type: ScoutMetricType.NUMBER,
-        invoke: (...metrics: Array<Metric>) => reduce(metrics, (acc: Metric, metric) => {
-            if (metric.value > -1 && acc.value == -1) acc.value = metric.value
-            else if (metric.value < acc.value) acc.value = metric.value
-            return acc
-          }, {type: ScoutMetricType.NUMBER, value: -1, category: metrics[0].category})
+        invoke: (...metrics: Array<Metric>) => ({
+            type: ScoutMetricType.NUMBER,
+            value: min(metrics.map((metric) => metric.value)),
+            category: metrics[0].category,
+        }),
     },
     'Average': {
         type: ScoutMetricType.NUMBER,
-        invoke: (...metrics: Array<Metric>) => ({type:ScoutMetricType.NUMBER, value: round(reduce(metrics, (acc, metric) => { 
-            return acc + metric.value}, 0) / metrics.length, 2), category: metrics[0].category
-        })
+        invoke: (...metrics: Array<Metric>) => ({
+            type: ScoutMetricType.NUMBER,
+            value: mean(metrics.map((metric) => metric.value)),
+            category: metrics[0].category,
+        }),
     },
     'Standard Deviation': {
         type: ScoutMetricType.NUMBER,
-        invoke: (...metrics: Array<Metric>) => ({type: ScoutMetricType.NUMBER, value: round(std(reduce(metrics, (acc, metric) => { 
-            acc.push(metric.value)
-            return acc
-            }, [])), 2), category: metrics[0].category})
+        invoke: (...metrics: Array<Metric>) => ({
+            type: ScoutMetricType.NUMBER,
+            value: std(metrics.map((metric) => metric.value)),
+            category: metrics[0].category,
+        }),
     },
     'Median': {
         type: ScoutMetricType.NUMBER,
-        invoke: (...metrics: Array<Metric>) => ({type: ScoutMetricType.NUMBER, value: median(reduce(metrics, (acc, metric) => {
-            acc.push(metric.value)
-            return acc
-        }, [])), category: metrics[0].category})
+        invoke: (...metrics: Array<Metric>) => ({
+            type: ScoutMetricType.NUMBER,
+            value: median(metrics.map((metric) => metric.value)),
+            category: metrics[0].category,
+        }),
     },
     'Mode': {
         type: ScoutMetricType.NUMBER,
-        invoke: (...metrics: Array<Metric>) => ({type: ScoutMetricType.NUMBER, value: mode(reduce(metrics, (acc, metric) => {
-            acc.push(metric.value)
-            return acc
-        }, [])), category: metrics[0].category})
+        invoke: (...metrics: Array<Metric>) => ({
+            type: ScoutMetricType.NUMBER,
+            value: mode(metrics.map((metric) => metric.value)),
+            category: metrics[0].category,
+        }),
     }
 };
